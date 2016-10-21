8b2fd9fa9d7a35df82511f284a0115cd47023286
Scott McConkey <scottmcconkey7@gmail.com>
2016-10-20 21:16:43 -0600

    Add project files.

---
+++ PowerBallSimulator/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PowerBallSimulator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void newgame_Click(object sender, RoutedEventArgs e)
+        {
+            // Set Powerball values
+            PowerBall white1 = new White();
+            this.Actual1.Text = white1.Number.ToString();
+
+            PowerBall white2 = new White();
+            this.Actual2.Text = white2.Number.ToString();
+
+            PowerBall white3 = new White();
+            this.Actual3.Text = white3.Number.ToString();
+
+            PowerBall white4 = new White();
+            this.Actual4.Text = white4.Number.ToString();
+
+            PowerBall white5 = new White();
+            this.Actual5.Text = white5.Number.ToString();
+
+            // Set Red Powerball
+            PowerBall red = new Red();
+            this.Actual6.Text = red.Number.ToString();
+        }
+
+        private void play_Click(object sender, RoutedEventArgs e)
+        {
+            // Set Guess values
+            PowerBall white1 = new White();
+            this.Guess1.Text = white1.Number.ToString();
+
+            PowerBall white2 = new White();
+            this.Guess2.Text = white2.Number.ToString();
+
+            PowerBall white3 = new White();
+            this.Guess3.Text = white3.Number.ToString();
+
+            PowerBall white4 = new White();
+            this.Guess4.Text = white4.Number.ToString();
+
+            PowerBall white5 = new White();
+            this.Guess5.Text = white5.Number.ToString();
+
+            // Set Red Powerball
+            PowerBall red = new Red();
+            this.Guess6.Text = red.Number.ToString();
+        }
+    }
+}
---
+++ PowerBallSimulator/PowerCalls.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerBallSimulator
+{
+    public abstract class PowerBall
+    {
+        // Establish a random seed for each ball
+        protected Random random = RandomNumber.GetRandomSeed();
+        protected int upperBound;
+        public string Color { get; set; }
+        public int Number { get; set; }
+    }
+
+    public class White : PowerBall
+    {
+        public White()
+        {
+            upperBound = 70;
+            Color = "white";
+            Number = random.Next(1, upperBound);
+        }
+    }
+
+    public class Red : PowerBall
+    {
+        public Red()
+        {
+            upperBound = 27;
+            Color = "red";
+            Number = random.Next(1, upperBound);
+        }
+    }
+
+    class Game
+    {
+        Game()
+        {
+
+        }
+    }
+
+    /// <summary>
+	/// Every power ball needs a random number within its appropriate range.
+    /// Use this function to generate a random seed that can be used by each
+    /// Ball object. The object will determine the range.
+	/// </summary>
+    class RandomNumber
+    {
+        public static Random GetRandomSeed()
+        {
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            return random;
+        }
+    }
+}
